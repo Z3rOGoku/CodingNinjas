6ed8f114e9a4918746f4c5eb70d25e980cce5511
Z3rOGoku <gaminguruji1015477365@gmail.com>
2022-06-06 19:02:56 +0530

    new

--- CodingNinjas_C++/Coding Pattern 2/i.cpp
+++ CodingNinjas_C++/Coding Pattern 2/i.cpp
@@ -1,36 +1,8 @@
 #include<iostream>
-
 using namespace std;
 
-int main()
-{
-    char ch;
-    ch=cin.get();
-
-    int alpha=0;
-    int space=0;
-    int number=0;
-
-    while(ch!='$')
-    {
-        
-
-        if ('a'<= ch && ch<='z')
-        {
-            alpha++;
-        }
-        else if ('0'<=ch && ch<='9')
-        {
-            number++;
-        }
-        else if (ch==' '|| ch=='\t' || ch=='\n')
-        {
-            space++;
-        }
-        ch = cin.get();
-
-
-    }
-
-    cout<<alpha<<" "<<number<<" "<<space;
+int main() {
+	// Write your code here
+	int n;
+    cin>>n
 }
